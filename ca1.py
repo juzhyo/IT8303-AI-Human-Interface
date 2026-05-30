e6c121ce121de7afc1829e0e81656154c7ce66ab
Justin Zhou <juzhyo@gmail.com>
2026-05-30 16:33:54 +0800

    Reduce trials from 35 to 30

--- ca1.py
+++ ca1.py
@@ -466,7 +466,7 @@ best_val_acc = 0.0
 best_params = {}
 
 tuning_epochs = 10 
-num_random_trials = 35 
+num_random_trials = 30 
 
 print(f"Starting Random Search: Testing {num_random_trials} combinations...\n")
 
